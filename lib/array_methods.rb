9542e083d285855a01542b1434c1e0ae96cbfba0
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-08-23 17:42:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,6 +15,13 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  min_value = 
+  min_value = 999999999
+  array.each do |x|
+    if x < min_value
+      min_value = x
+      
+    end
+  end
+  return min_value
   
 end
